3e6856191596f7a2960a01066fe61f5a76d65a55
Antoni Morawski <ant.morawski@gmail.com>
2023-11-16 23:43:46 +0100

    feat: add star icons

---
+++ src/assets/StarCheckedIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const StarCheckedIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+      <path
+        d="m12 17.8 6.2 3.7-1.7-7L22 9.7l-7.2-.6L12 2.5 9.2 9.1 2 9.7l5.5 4.8-1.7 7 6.2-3.7Z"
+        fill="#F2C94C"
+      />
+    </svg>
+  );
+};
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import { getUsers } from "../services/getUsersService";
 import { SearchIcon } from "../assets/SearchIcon";
 import { User } from "../types";
+import { StarUncheckedIcon } from "../assets/StarUncheckedIcon";
+import { StarCheckedIcon } from "../assets/StarCheckedIcon";
 
 export const Search = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [searchInput, setSearchInput] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
 
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value);
@@ -20,6 +23,7 @@ export const Search = () => {
     fetchData();
   }, [searchInput]);
 
+  // todo: refactor this
   const x = users && Object.values(users).map((user) => <p>{user.login}</p>);
 
   console.log(users);
@@ -27,13 +31,16 @@ export const Search = () => {
   return (
     <>
       <div className="header">
-        <div className="container">
+        <div className="header__container">
           <SearchIcon />
           <input
-            className="container__input"
+            className="header__input"
             placeholder="Search for GitHub users..."
             onChange={inputHandler}
           />
+          <div className="header__star" onClick={() => setIsChecked(!isChecked)}>
+            {isChecked ? <StarCheckedIcon /> : <StarUncheckedIcon />}
+          </div>
         </div>
       </div>
 
